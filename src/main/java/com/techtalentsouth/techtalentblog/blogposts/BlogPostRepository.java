a6aa55749b439f19e3b32b4ddc90bce4a77335fd
hbaraket <hbaraket@uncc.edu>
2020-11-25 17:42:49 -0500

    first commit

---
+++ src/main/java/com/techtalentsouth/techtalentblog/blogposts/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.techtalentsouth.techtalentblog.blogposts;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+    
+}
